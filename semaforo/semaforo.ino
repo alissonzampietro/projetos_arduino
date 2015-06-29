6b17c2f43acdd5ae2f6f477911ba22efb86ed4ab
alissonzampietro <alisson_zam@hotmail.com>
2015-06-29 11:02:46 -0300

    Adicionando comentários no projeto semáforos

--- semaforo/semaforo.ino
+++ semaforo/semaforo.ino
@@ -1,14 +1,19 @@
-int led = 4, flag = 0,delays[] = {20000,4000,10000};
+int led = 4, // variável que representa as portas digitais utilizadas
+    flag = 0, // variável flag para saber quando mudar as luzes
+    delays[] = {20000,4000,10000}; // array com tempos dos leds do semáforo
 void setup() {
+  // informa quais portas serão usadas
   pinMode(4,OUTPUT);
   pinMode(8,OUTPUT);
   pinMode(12,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(led,HIGH);
-  delay(delays[flag]);
-  digitalWrite(led,LOW);
+  digitalWrite(led,HIGH); // liberamos energia para o pino representado na variável LED
+  delay(delays[flag]); // utilizamos a posição do flag para localizar quanto tempo deve demorar para a luz apagar
+  digitalWrite(led,LOW); // cortamos energia para o pino representado na variável LED
+  
+  //  realizamos cálculos para que se possa mudar as luzes do led
   if(flag<2){
     flag = flag + 1;
     led = led + 4;
