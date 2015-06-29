bbc6e4568767658abe7c01da846003c0e81249a8
Alisson Zampietro <alisson@mentesnotaveis.com.br>
2015-06-29 14:49:32 -0300

    Implementação do semaforo de pedestre

--- semaforo/semaforo.ino
+++ semaforo/semaforo.ino
@@ -1,18 +1,40 @@
 int led = 4, // variável que representa as portas digitais utilizadas
     flag = 0, // variável flag para saber quando mudar as luzes
-    delays[] = {20000,4000,10000}; // array com tempos dos leds do semáforo
+    pedestrePassa = 2,
+    pedestreNaoPassa = 1,
+    delays[] = {10000,4000,10000}; // array com tempos dos leds do semáforo
 void setup() {
   // informa quais portas serão usadas
+  pinMode(pedestreNaoPassa,OUTPUT);
+  pinMode(pedestrePassa,OUTPUT);
   pinMode(4,OUTPUT);
   pinMode(8,OUTPUT);
   pinMode(12,OUTPUT);
 }
 
 void loop() {
+  if(flag == 0){
+    digitalWrite(pedestrePassa,LOW);
+    digitalWrite(pedestreNaoPassa,LOW);
+    digitalWrite(12,HIGH);
+     for(int i = 0;i<5;i++){
+      digitalWrite(pedestreNaoPassa,HIGH);
+      delay(1000);
+      digitalWrite(pedestreNaoPassa,LOW);
+      delay(1000);
+    }
+    digitalWrite(pedestreNaoPassa,HIGH);
+    digitalWrite(12,LOW);
+  }
+  
   digitalWrite(led,HIGH); // liberamos energia para o pino representado na variável LED
+   if(flag == 2){
+    digitalWrite(pedestreNaoPassa,LOW);
+    delay(800);
+    digitalWrite(pedestrePassa,HIGH);
+  }
   delay(delays[flag]); // utilizamos a posição do flag para localizar quanto tempo deve demorar para a luz apagar
   digitalWrite(led,LOW); // cortamos energia para o pino representado na variável LED
-  
   //  realizamos cálculos para que se possa mudar as luzes do led
   if(flag<2){
     flag = flag + 1;
